2fe3ea8ee6a8ca58f0023b7489409898b2f5ba0a
SingularDave <106774543+SingularDave@users.noreply.github.com>
2022-11-05 14:10:30 -0500

    Added exportPathMap to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 
+// Add exportPathMap to next.config.js
 module.exports = {
     experimental: {
         runtime: 'experimental-edge'
+    },
+    exportPathMap: async function () {
+        const paths = {
+            '/': { page: '/' }
+        };
+        const res = await fetch('http://localhost:3000/api/get_employees');
+        const data = await res.json();
+        data.forEach((employee) => {
+            paths[`/employee/${employee.id}`] = { page: '/employee/[id]', query: { id: employee.id } };
+        });
+        return paths;
     }
 }
