099c3960de0b561542e77ba7cb5d5525b224f523
SingularDave <106774543+SingularDave@users.noreply.github.com>
2022-11-05 11:33:19 -0500

    Added getStaticProps to SearchSection.js and added api directory.

--- src/pages/api/get_employees.js
+++ src/pages/api/get_employees.js
@@ -1,15 +1,18 @@
 import {all_providers} from "./employees";
 
+
 export const config = {
     runtime: 'experimental-edge',
 }
 
-export default async function handler(req, res) {
-    return new Response(JSON.stringify(all_providers), {
-        status: 200,
-        headers: {
-            'content-type': 'application/json',
-        },
-    })
+export default async function () {
+    return new Response(
+        JSON.stringify(all_providers),
+        {
+            status: 200,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+    )
 }
-
