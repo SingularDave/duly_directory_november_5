9de45b23ff0563b3175842bed2fbe847cbbe543d
SingularDave <106774543+SingularDave@users.noreply.github.com>
2022-11-05 11:55:17 -0500

    Commented out getStaticProps in SearchSection.js.

--- src/pages/api/get_employees.js
+++ src/pages/api/get_employees.js
@@ -1,18 +1,18 @@
 import {all_providers} from "./employees";
 
 
-export const config = {
-    runtime: 'experimental-edge',
-}
-
-export default async function () {
-    return new Response(
-        JSON.stringify(all_providers),
-        {
-            status: 200,
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        }
-    )
-}
+// export const config = {
+//     runtime: 'experimental-edge',
+// }
+//
+// export default async function () {
+//     return new Response(
+//         JSON.stringify(all_providers),
+//         {
+//             status: 200,
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             }
+//         }
+//     )
+// }
