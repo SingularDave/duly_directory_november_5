98bfafd3c3240096dc34de32f825e37e792c7008
SingularDave <106774543+SingularDave@users.noreply.github.com>
2022-11-05 14:37:38 -0500

    Updated Functions with OnRequest

--- functions/api/get_employees.js
+++ functions/api/get_employees.js
@@ -1,15 +1,6 @@
 import {all_providers} from "./employees";
 
-// add event listener to the serverless function
-addEventListener("fetch", event => {
-    event.respondWith(handleRequest(event.request));
-});
-
-// Make handleRequest to respond to http request. Return json of all_providers.
-async function handleRequest() {
-    return new Response(JSON.stringify(all_providers), {
-        headers: {
-            "content-type": "application/json;charset=UTF-8",
-        },
-    });
+// OnRequest is a function that takes a request and returns a response. Response will be all_providers as JSON.
+export const OnRequest = (req) => {
+    return all_providers;
 }
