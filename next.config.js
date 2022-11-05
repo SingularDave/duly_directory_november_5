0b32e17e8dde20f5d09b8ffef8c90fb3c1f860d7
SingularDave <106774543+SingularDave@users.noreply.github.com>
2022-11-05 12:02:18 -0500

    Nuked next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    experimental: {
-        runtime: 'experimental-edge'},
-reactStrictMode: true,
-    swcMinify: true
-}
-
-module.exports = nextConfig
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     experimental: {
+//         runtime: 'experimental-edge'},
+// reactStrictMode: true,
+//     swcMinify: true
+// }
+//
+// module.exports = nextConfig
