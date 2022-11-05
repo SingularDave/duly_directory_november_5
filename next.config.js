4fa08fbae0ed030a40c45f8b6d5ce8561e10caf6
SingularDave <106774543+SingularDave@users.noreply.github.com>
2022-11-05 14:16:50 -0500

    Changed next.config.js to CF recommendation.

--- next.config.js
+++ next.config.js
@@ -1,18 +1,11 @@
 
-// Add exportPathMap to next.config.js
-module.exports = {
+
+const nextConfig = {
     experimental: {
         runtime: 'experimental-edge'
     },
-    exportPathMap: async function () {
-        const paths = {
-            '/': { page: '/' }
-        };
-        const res = await fetch('http://localhost:3000/api/get_employees');
-        const data = await res.json();
-        data.forEach((employee) => {
-            paths[`/employee/${employee.id}`] = { page: '/employee/[id]', query: { id: employee.id } };
-        });
-        return paths;
-    }
+    reactStrictMode: true,
+    swcMinify: true
 }
+
+module.exports = nextConfig
