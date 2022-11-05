71e6bcafc778a41e8c38ad9e5cbae334f2d5f83a
SingularDave <106774543+SingularDave@users.noreply.github.com>
2022-11-05 14:39:04 -0500

    Updated Functions with OnRequest

--- functions/api/get_employees.js
+++ functions/api/get_employees.js
@@ -1,6 +1,6 @@
 import {all_providers} from "./employees";
 
 // OnRequest is a function that takes a request and returns a response. Response will be all_providers as JSON.
-export const OnRequest = (req) => {
+export const OnRequest = () => {
     return all_providers;
 }
